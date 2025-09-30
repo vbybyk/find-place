2301ffcb0c2bfd063341b5e9e02399b9efaf738c
vbybyk <vitaliy.bybyk@gmail.com>
2025-09-30 20:47:38 +0800

    fix: Fixed issue with key in autocomplete options, added _ rule to eslint

--- src/components/common/autocomplete/index.tsx
+++ src/components/common/autocomplete/index.tsx
@@ -120,12 +120,12 @@ const MuiAutocomplete = forwardRef(function Autocomplete(
             className="text-sm box-border p-1.5 my-3 mx-0 min-w-[320px] rounded-xl overflow-auto outline-0 max-h-[300px] z-[1] bg-white dark:bg-gray-800 border border-solid border-gray-200 dark:border-gray-900 text-gray-900 dark:text-gray-200 shadow-[0_4px_30px_transparent] shadow-gray-200 dark:shadow-gray-900"
           >
             {(groupedOptions as IOption[]).map((option, index) => {
-              const optionProps = getOptionProps({ option, index });
+              const { key: _key, ...optionProps } = getOptionProps({ option, index });
 
               return (
                 <li
-                  {...optionProps}
                   key={`${option.id}-${index}`}
+                  {...optionProps}
                   className="list-none p-2 rounded-lg cursor-default last-of-type:border-b-0 hover:cursor-pointer aria-selected:bg-violet-100 dark:aria-selected:bg-violet-900 aria-selected:text-violet-900 dark:aria-selected:text-violet-100 ui-focused:bg-gray-100 dark:ui-focused:bg-gray-700 ui-focus-visible:bg-gray-100 dark:ui-focus-visible:bg-gray-800 ui-focused:text-gray-900 dark:ui-focused:text-gray-300 ui-focus-visible:text-gray-900 dark:ui-focus-visible:text-gray-300 ui-focus-visible:shadow-[0_0_0_3px_transparent] ui-focus-visible:shadow-violet-200 dark:ui-focus-visible:shadow-violet-500 ui-focused:aria-selected:bg-violet-100 dark:ui-focused:aria-selected:bg-violet-900 ui-focus-visible:aria-selected:bg-violet-100 dark:ui-focus-visible:aria-selected:bg-violet-900 ui-focused:aria-selected:text-violet-900 dark:ui-focused:aria-selected:text-violet-100 ui-focus-visible:aria-selected:text-violet-900 dark:ui-focus-visible:aria-selected:text-violet-100"
                 >
                   {option.label}
