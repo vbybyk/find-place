152bbf36a9cb37cbccae6f82aea0148d8680b390
vbybyk <vitaliy.bybyk@gmail.com>
2025-10-02 20:16:30 +0800

    Replaced mui/base with base-ui, updated Select component

--- src/components/common/select/index.tsx
+++ src/components/common/select/index.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import React, { forwardRef } from "react";
-import { Select as BaseSelect, SelectRootSlotProps, SelectProps, SelectListboxSlotProps } from "@mui/base/Select";
-import { Option as BaseOption, OptionProps, OptionOwnerState } from "@mui/base/Option";
+import { Select as BaseUISelect } from "@base-ui-components/react/select";
 import clsx from "clsx";
-import { PopupContext } from "@mui/base/Unstable_Popup";
-import { CssTransition } from "@mui/base/Transitions";
 
 interface ISelectProps {
   value: any;
@@ -15,168 +12,91 @@ interface ISelectProps {
   placeholder?: string;
 }
 
-const Select = forwardRef<HTMLDivElement, ISelectProps>((props: ISelectProps, ref) => {
-  const { options, keyValue, ...rest } = props;
+const Select = forwardRef<HTMLButtonElement, ISelectProps>((props, ref) => {
+  const { options, keyValue, value, onChange, placeholder } = props;
+
+  const items = options.map((option) => ({
+    label: option.name,
+    value: keyValue ? option[keyValue] : option.id,
+  }));
 
   return (
-    <div ref={ref}>
-      <MuiSelect options={options} keyValue={keyValue} {...rest}>
-        {options.map((option) => (
-          <Option key={option.id} value={keyValue ? option[keyValue] : option.id}>
-            {option.name}
-          </Option>
-        ))}
-      </MuiSelect>
-    </div>
+    <BaseUISelect.Root value={value} onValueChange={(newValue) => onChange?.(newValue)} items={items}>
+      <BaseUISelect.Trigger
+        ref={ref}
+        className="flex items-center justify-between text-sm font-sans box-border w-80 h-10 px-3 py-2 rounded-lg text-left bg-white dark:bg-neutral-900 border border-solid border-slate-200 dark:border-neutral-700 text-slate-900 dark:text-neutral-300 transition-all hover:bg-slate-50 dark:hover:bg-neutral-800 outline-0 shadow-md shadow-slate-100 dark:shadow-slate-900 focus-visible:ring-4 focus-visible:ring-purple-500/30 focus-visible:border-purple-500 focus-visible:dark:border-purple-500"
+      >
+        {value ? <BaseUISelect.Value /> : <span className="text-slate-900 dark:text-slate-300">{placeholder}</span>}
+        <BaseUISelect.Icon className="flex">
+          <ChevronUpDownIcon />
+        </BaseUISelect.Icon>
+      </BaseUISelect.Trigger>
+
+      <BaseUISelect.Portal>
+        <BaseUISelect.Positioner sideOffset={-12} alignItemWithTrigger={false}>
+          <BaseUISelect.Popup
+            className={() =>
+              clsx(
+                "text-sm font-sans p-1.5 my-3 w-80 rounded-xl overflow-auto outline-0 bg-white dark:bg-slate-900 border border-solid border-slate-200 dark:border-slate-700 text-slate-900 dark:text-slate-300 shadow shadow-slate-200 dark:shadow-slate-900 transition-[opacity,transform]"
+              )
+            }
+          >
+            {items.map((item) => (
+              <BaseUISelect.Item
+                key={item.value}
+                value={item.value}
+                className={({ selected, highlighted, disabled }) =>
+                  clsx(
+                    "flex items-center list-none p-2 rounded-lg cursor-default last-of-type:border-b-0",
+                    disabled && "text-slate-400 dark:text-slate-700",
+                    !disabled && selected && "bg-purple-100 dark:bg-purple-950 text-purple-950 dark:text-purple-50",
+                    !disabled &&
+                      highlighted &&
+                      !selected &&
+                      "bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-300",
+                    !disabled &&
+                      "hover:bg-slate-100 hover:dark:bg-slate-800 hover:text-slate-900 hover:dark:text-slate-300",
+                    !disabled &&
+                      "focus-visible:outline focus-visible:outline-2 focus-visible:outline-purple-400 focus-visible:dark:outline-purple-300"
+                  )
+                }
+              >
+                <BaseUISelect.ItemText>{item.label}</BaseUISelect.ItemText>
+                <BaseUISelect.ItemIndicator className="ml-auto">
+                  <CheckIcon />
+                </BaseUISelect.ItemIndicator>
+              </BaseUISelect.Item>
+            ))}
+          </BaseUISelect.Popup>
+        </BaseUISelect.Positioner>
+      </BaseUISelect.Portal>
+    </BaseUISelect.Root>
   );
 });
 
 Select.displayName = "Select";
 
-const getOptionColorClasses = ({ selected, highlighted, disabled }: Partial<OptionOwnerState<number>>) => {
-  let classes = "";
-  if (disabled) {
-    classes += "text-slate-400 dark:text-slate-700";
-  } else {
-    if (selected) {
-      classes += " bg-purple-100 dark:bg-purple-950 text-purple-950 dark:text-purple-50";
-    } else if (highlighted) {
-      classes += " bg-slate-100 dark:bg-slate-800 text-slate-900 dark:text-slate-300";
-    }
-    classes += " hover:dark:bg-slate-800 hover:bg-slate-100 hover:dark:text-slate-300 hover:text-slate-900";
-    classes +=
-      " focus-visible:outline focus-visible:outline-2 focus-visible:outline-purple-400 focus-visible:dark:outline-purple-300";
-  }
-  return classes;
-};
-
-const Option = React.forwardRef<HTMLLIElement, OptionProps<number>>((props, ref) => {
+function ChevronUpDownIcon() {
   return (
-    <BaseOption
-      ref={ref}
-      {...props}
-      slotProps={{
-        root: ({ selected, highlighted, disabled }) => ({
-          className: `list-none p-2 rounded-lg cursor-default last-of-type:border-b-0 ${getOptionColorClasses({
-            selected,
-            highlighted,
-            disabled,
-          })}`,
-        }),
-      }}
-    />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+    </svg>
   );
-});
+}
 
-Option.displayName = "Option";
-
-const Button = React.forwardRef(function Button<TValue extends {}, Multiple extends boolean>(
-  props: SelectRootSlotProps<TValue, Multiple>,
-  ref: React.ForwardedRef<HTMLButtonElement>
-) {
-  const { ownerState, ...other } = props;
+function CheckIcon() {
   return (
-    <button type="button" {...other} ref={ref}>
-      {other.children}
-      {/* <UnfoldMoreRoundedIcon /> */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-      </svg>
-    </button>
+    <svg fill="currentcolor" width="10" height="10" viewBox="0 0 10 10">
+      <path d="M9.1603 1.12218C9.50684 1.34873 9.60427 1.81354 9.37792 2.16038L5.13603 8.66012C5.01614 8.8438 4.82192 8.96576 4.60451 8.99384C4.3871 9.02194 4.1683 8.95335 4.00574 8.80615L1.24664 6.30769C0.939709 6.02975 0.916013 5.55541 1.19372 5.24822C1.47142 4.94102 1.94536 4.91731 2.2523 5.19524L4.36085 7.10461L8.12299 1.33999C8.34934 0.993152 8.81376 0.895638 9.1603 1.12218Z" />
+    </svg>
   );
-});
-
-const AnimatedListbox = React.forwardRef(function AnimatedListbox<Value extends {}, Multiple extends boolean>(
-  props: SelectListboxSlotProps<Value, Multiple>,
-  ref: React.ForwardedRef<HTMLUListElement>
-) {
-  const { ownerState, ...other } = props;
-  const popupContext = React.useContext(PopupContext);
-
-  if (popupContext == null) {
-    throw new Error("The `AnimatedListbox` component cannot be rendered outside a `Popup` component");
-  }
-
-  const verticalPlacement = popupContext.placement.split("-")[0];
-
-  return (
-    <CssTransition className={`placement-${verticalPlacement}`} enterClassName="open" exitClassName="closed">
-      <ul {...other} ref={ref} />
-    </CssTransition>
-  );
-});
-
-const resolveSlotProps = (fn: any, args: any) => (typeof fn === "function" ? fn(args) : fn);
-
-const MuiSelect = React.forwardRef(function CustomSelect<TValue extends {}, Multiple extends boolean>(
-  props: SelectProps<TValue, Multiple>,
-  ref: React.ForwardedRef<HTMLButtonElement>
-) {
-  const { options, keyValue, onChange, ...rest } = props as ISelectProps;
-  const mappedOptions = options?.map((option) => ({
-    ...option,
-    value: keyValue ? option[keyValue] : option,
-  }));
-
-  return (
-    <BaseSelect
-      ref={ref}
-      {...rest}
-      options={mappedOptions}
-      onChange={(event, value) => {
-        if (!!onChange) {
-          onChange(value);
-        }
-      }}
-      slots={{
-        root: Button,
-        listbox: AnimatedListbox,
-        ...props.slots,
-      }}
-      className={clsx("CustomSelect", props.className)}
-      slotProps={{
-        ...props.slotProps,
-        root: (ownerState) => {
-          const resolvedSlotProps = resolveSlotProps(props.slotProps?.root, ownerState);
-          return {
-            ...resolvedSlotProps,
-            className: clsx(
-              `relative text-sm font-sans box-border w-80 px-3 py-2 rounded-lg text-left bg-white dark:bg-neutral-900 border border-solid border-slate-200 dark:border-neutral-700 text-slate-900 dark:text-neutral-300 transition-all hover:bg-slate-50 dark:hover:bg-neutral-800 outline-0 shadow-md shadow-slate-100 dark:shadow-slate-900 ${
-                ownerState.focusVisible
-                  ? "focus-visible:ring-4 ring-purple-500/30 focus-visible:border-purple-500 focus-visible:dark:border-purple-500"
-                  : ""
-              } [&>svg]:text-base	[&>svg]:absolute [&>svg]:h-full [&>svg]:top-0 [&>svg]:right-2.5`,
-              resolvedSlotProps?.className
-            ),
-          };
-        },
-        listbox: (ownerState) => {
-          const resolvedSlotProps = resolveSlotProps(props.slotProps?.listbox, ownerState);
-          return {
-            ...resolvedSlotProps,
-            className: clsx(
-              `text-sm font-sans p-1.5 my-3 w-80 rounded-xl overflow-auto outline-0 bg-white dark:bg-slate-900 border border-solid border-slate-200 dark:border-slate-700 text-slate-900 dark:text-slate-300 shadow shadow-slate-200 dark:shadow-slate-900 [.open_&]:opacity-100 [.open_&]:scale-100 transition-[opacity,transform] [.closed_&]:opacity-0 [.closed_&]:scale-90 [.placement-top_&]:origin-bottom [.placement-bottom_&]:origin-top`,
-              resolvedSlotProps?.className
-            ),
-          };
-        },
-        popup: (ownerState) => {
-          const resolvedSlotProps = resolveSlotProps(props.slotProps?.popup, ownerState);
-          return {
-            ...resolvedSlotProps,
-            className: clsx(`z-10`, resolvedSlotProps?.className),
-          };
-        },
-      }}
-    />
-  );
-});
+}
 
 export default Select;
